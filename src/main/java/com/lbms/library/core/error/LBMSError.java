7a1c249d6bd8d0308a2eb2a5c5a9891007eddeba
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-02-18 23:49:26 +0530

    Added error message

--- src/main/java/com/lbms/library/core/error/LBMSError.java
+++ src/main/java/com/lbms/library/core/error/LBMSError.java
@@ -2,7 +2,8 @@ package com.lbms.library.core.error;
 
 public enum LBMSError implements ThrowableError{
     MEMBER_EXISTS("LBMSVAL0001","Member exists with the email address"),
-    MEMBER_DOES_NOT_EXIST("LBMSVAL0002","A member with the user id does not exist");
+    MEMBER_DOES_NOT_EXIST("LBMSVAL0002","A member with the user id does not exist"),
+    INVALID_MEMBER_TO_UPDATE("LBMSVAL0003","A member with the requested user ID doesn't exist to update");
 
     LBMSError(String code, String message) {
         this.code = code;
