07f1ee45945ba21bc3c3b0c3a242e9a0999bff14
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-09 00:13:44 +0530

    Added Validation Error Code

--- src/main/java/com/lbms/library/core/error/LBMSError.java
+++ src/main/java/com/lbms/library/core/error/LBMSError.java
@@ -4,7 +4,8 @@ public enum LBMSError implements ThrowableError{
     MEMBER_EXISTS("LBMSVAL0001","Member exists with the email address"),
     MEMBER_DOES_NOT_EXIST("LBMSVAL0002","A member with the user id does not exist"),
     INVALID_MEMBER_TO_UPDATE("LBMSVAL0003","A member with the requested user ID doesn't exist to update"),
-    EMAIL_CANNOT_BE_UPDATED("LBMSVAL0004","Email address cannot be changed for a already registered user");
+    EMAIL_CANNOT_BE_UPDATED("LBMSVAL0004","Email address cannot be changed for a already registered user"),
+    CATEGORY_EXISTS("LBMSVALCAT0001","Category already exists");
 
     LBMSError(String code, String message) {
         this.code = code;
