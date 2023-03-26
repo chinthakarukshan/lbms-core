a8fffb774e466362c19d87125da0f886b34b60db
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-27 00:28:18 +0530

    Added DTO for Category Details

---
+++ src/main/java/com/lbms/library/core/dto/category/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.lbms.library.core.dto.category;
+
+import jdk.jfr.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO extends CategorySummaryDTO {
+
+    private String updatedBy;
+
+    private Date updatedDate;
+}
