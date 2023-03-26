6cde1d92db46e905c74d83b0de7b42666bf9cd4a
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-27 01:08:40 +0530

    Introduced error code for category flow

--- src/main/java/com/lbms/library/core/error/LBMSError.java
+++ src/main/java/com/lbms/library/core/error/LBMSError.java
@@ -5,7 +5,8 @@ public enum LBMSError implements ThrowableError{
     MEMBER_DOES_NOT_EXIST("LBMSVAL0002","A member with the user id does not exist"),
     INVALID_MEMBER_TO_UPDATE("LBMSVAL0003","A member with the requested user ID doesn't exist to update"),
     EMAIL_CANNOT_BE_UPDATED("LBMSVAL0004","Email address cannot be changed for a already registered user"),
-    CATEGORY_EXISTS("LBMSVALCAT0001","Category already exists");
+    CATEGORY_EXISTS("LBMSVALCAT0001","Category already exists"),
+    CATEGORY_WITH_ID_DOES_NOT_EXIST("LBMSVALCAT0002", "Requested category Id does not exist.");
 
     LBMSError(String code, String message) {
         this.code = code;
