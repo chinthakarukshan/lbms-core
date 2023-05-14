e14cb971f85b4c0dc11bacdd8c93c15419348722
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-05-15 00:14:27 +0530

    lbms-admin-service#17 Added DTO for Author Summary

---
+++ src/main/java/com/lbms/library/core/dto/author/AuthorSummaryDTO.java
@@ -0,0 +1,16 @@
+package com.lbms.library.core.dto.author;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthorSummaryDTO {
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+}
